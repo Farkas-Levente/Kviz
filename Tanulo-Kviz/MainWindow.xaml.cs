a73dcb44af340f2da48104b0d1893d04515a9e78
Farkas-Levente <89079547+Farkas-Levente@users.noreply.github.com>
2022-01-31 19:27:02 +0100

    Tantargy és téma class
    A 2 class majdnem kész, még nincs tesztelve

--- Tanulo-Kviz/MainWindow.xaml.cs
+++ Tanulo-Kviz/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +19,73 @@ namespace Tanulo_Kviz
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
+
+        public Dictionary<string, Tantargy> tantargyNyilvantarto = new Dictionary<string, Tantargy>();
+        public List<Tantargy> targyak = new List<Tantargy>();
         public MainWindow()
         {
+
             InitializeComponent();
+
+        }
+
+
+
+
+
+
+
+
+        public class Tantargy
+        {
+            List<Tema> temak = new List<Tema>();
+            string nev;
+
+            public Tantargy(string eleresiUt)
+            {
+                string[] allomany = File.ReadAllLines(eleresiUt);
+                nev = allomany[0].Split(';')[0];
+
+
+                List<string> temaFajtak = new List<string>();
+                foreach (string sor in allomany)
+                {
+                    string temaNev = sor.Split(';')[1];
+                    if (!temaFajtak.Contains(temaNev))
+                    {
+                        temaFajtak.Add(temaNev);
+                        Tema ujTema = new Tema(temaNev,allomany);
+                        temak.Add(ujTema);
+                    }
+                }
+            }
+        }
+
+        public class Tema
+        {
+            List<Kerdes> kerdesek = new List<Kerdes>();
+            string nev;
+            public Tema(string nev,string[] allomany)
+            {
+                this.nev = nev;
+                foreach(string sor in allomany)
+                {
+                    if(sor.Split(';')[1] == nev)
+                    {
+                        Kerdes kerdes = new Kerdes();
+                        kerdesek.Add(kerdes);
+                    }
+                     
+                }
+            }
+        }
+
+        public class Kerdes
+        {
+            
         }
     }
 }
